25d79077bbe2fbc98ca822009a69a72904c56931
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-05-13 12:49:10 +0530

    Added signup logic and pwd encryption

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const {PORT} = require('./config/serverConfig')
 const bodyParser = require("body-parser");
-
+const apiRoutes = require('./routes/index');
+const app= express();
 
 const setupAndStartServer = async()=>{
-
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}))
     
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-    app.listen(PORT , ()=>{
-        console.log(`Server Started on Port no ${PORT}`)
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, async () => {
+        console.log(`Server Started on Port: ${PORT}`);
+        // const incomingpassword = '123456';
+        // const user = await User.findByPk(3);
+        // const response = bcrypt.compareSync(incomingpassword, user.password);
+        // console.log(response);
     })
 }
 
